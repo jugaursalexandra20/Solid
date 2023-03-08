5c4d2eda256b3c57c8694c210895b9925b06227b
jugaursalexandra20 <jugaursalexandra20@stud.ase.ro>
2023-03-08 09:12:22 +0200

    Added the following classes: TableCreator,DataInserter, DataReader

--- src/ro/ase/acs/main/Main.java
+++ src/ro/ase/acs/main/Main.java
@@ -1,5 +1,9 @@
 package ro.ase.acs.main;
 
+import ro.ase.acs.classes.DataInserter;
+import ro.ase.acs.classes.DataReader;
+import ro.ase.acs.classes.TableCreator;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,9 +18,14 @@ public class Main {
             Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db");
             connection.setAutoCommit(false);
 
-            createTable(connection);
-            insertData(connection);
-            readData(connection);
+            TableCreator createTable = new TableCreator();
+            createTable.createTable(connection);
+
+            DataInserter dataInserter = new DataInserter();
+            dataInserter.insertData(connection);
+
+            DataReader dataReader = new DataReader();
+            dataReader.readData(connection);
 
             connection.close();
         } catch (Exception e) {
@@ -24,52 +33,4 @@ public class Main {
         }
     }
 
-    private static void createTable(Connection connection) throws SQLException {
-        String sqlDrop = "DROP TABLE IF EXISTS employees";
-        String sqlCreate = "CREATE TABLE employees(id INTEGER PRIMARY KEY,"
-                + "name TEXT, address TEXT, salary REAL)";
-
-        Statement statement = connection.createStatement();
-        statement.executeUpdate(sqlDrop);
-        statement.executeUpdate(sqlCreate);
-        statement.close();
-        connection.commit();
-    }
-
-    private static void insertData(Connection connection) throws SQLException {
-        String sqlInsert = "INSERT INTO employees VALUES(1, 'Popescu Ion', 'Bucharest', 4000)";
-        Statement statement = connection.createStatement();
-        statement.executeUpdate(sqlInsert);
-        statement.close();
-
-        String sqlInsertWithParams = "INSERT INTO employees VALUES (?,?,?,?)";
-        PreparedStatement preparedStatement =
-                connection.prepareStatement(sqlInsertWithParams);
-        preparedStatement.setInt(1, 2);
-        preparedStatement.setString(2, "Ionescu Vasile");
-        preparedStatement.setString(3, "Brasov");
-        preparedStatement.setDouble(4, 4500);
-        preparedStatement.executeUpdate();
-        preparedStatement.close();
-
-        connection.commit();
-    }
-
-    private static void readData(Connection connection) throws SQLException {
-        String sqlSelect = "SELECT * FROM employees";
-        Statement statement = connection.createStatement();
-        ResultSet rs = statement.executeQuery(sqlSelect);
-        while(rs.next()) {
-            int id = rs.getInt("id");
-            System.out.println("id: " + id);
-            String name = rs.getString(2);
-            System.out.println("name: " + name);
-            String address = rs.getString("address");
-            System.out.println("address: " + address);
-            double salary = rs.getDouble("salary");
-            System.out.println("salary: " + salary);
-        }
-        rs.close();
-        statement.close();
-    }
 }
